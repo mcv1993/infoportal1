260f4bb44756e13f34a9548254a219148135c831
mcv1993 <mcv1993@gmail.com>
2019-10-06 19:53:50 +0000

    devise installed, models created, migration complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  devise_for :admins
+  devise_for :sas
+  devise_for :techs
+  get 'static_pages/index'
+  
+  root 'static_pages#index'
 end
